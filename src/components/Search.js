6da1b9d1401a4d56d54c182f91461234df57b77f
Dmitry <dslonkov@bk.ru>
2020-11-03 21:26:46 +0300

    initial commit

---
+++ src/components/Search.js
@@ -0,0 +1,37 @@
+import React, {useState, useContext} from 'react'
+import { AlertContext } from '../context/alert/AlertContext'
+import { GithubContext } from '../context/github/GithubContext'
+
+export const Search = () => {
+  const [value, setValue] = useState('')
+  const alert = useContext(AlertContext)
+  const github = useContext(GithubContext)
+
+  const onsubmit = event => {
+    if(event.key !== 'Enter') {
+      return 
+    }
+    
+    github.clearUsers()
+
+    if(value.trim()) {
+      alert.hide()
+      github.search(value.trim())
+    } else {
+      alert.show('Введите данные пользователя')
+    }
+  }
+
+  return (
+    <div className="form-group">
+      <input 
+        type='text'
+        className='form-control'
+        placeholder='Введите ник пользователя...'
+        onKeyPress={onsubmit}
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+    </div>
+  )
+}
